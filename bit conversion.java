51d2b37c8ff2d9ec35539923932d03de39ee1860
Rumela Dasgupta <dasgupta.rumela.official@gmail.com>
2025-09-18 10:54:22 +0530

    18.09.2025new2

---
+++ bit conversion.java
@@ -0,0 +1,5 @@
+//act2:write a program in java to convert all characters in file to corresponding bits.use command line arguments.
+//example:
+//suppose our input file name=myfile.txt which contains ABCD.
+// the output file name=bitfile.txt which contains 010000101000010100001101000100
+//c:\java\bin>java_bit_conversion myfile.txt bitfile.txt<enter>
